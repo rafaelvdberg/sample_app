7e211e5221976b8cad2f058eca71d8d31bcaeace
Rafael van den Berg <rafael@hotelwifi.com>
2011-07-12 22:34:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5df8b180d4e35a6ad88cd9ce87e019a4edc646962ebbca2baefe1b78a05cb684863e4304de51187f114a7c94a7e56c231bdac8ab03615dc3981a73632568a81b'
